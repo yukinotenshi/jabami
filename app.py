4c7afed96aa56d08f169725ecc0743111b91e349
yukinotenshi <gabriel.bentara@gmail.com>
2018-12-20 06:52:42 +0700

    fix static url + added requirements.txt

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from util import generate_random_string
 from background import cast_encoder
 
 
-app = Flask(__name__, static_folder='encoded')
+app = Flask(__name__, static_folder='encoded', static_url_path='/static')
 app.config['UPLOAD_DIR'] = "./upload"
 
 
